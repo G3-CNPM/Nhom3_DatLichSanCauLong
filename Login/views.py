3b8148e4afbaf8503e21bd9cb914ad2f8eb3036b
KhangUTH <khangtm2739@ut.edu.vn>
2024-12-21 12:57:11 +0700

    giao dien login & reg

--- Login/views.py
+++ Login/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def get_login(request):
     return render(request,'Login/login.html')
+
+def get_register(request):
+    return render(request,'Register/register.html')
--- mySite/urls.py
+++ mySite/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from DLSCL import views as home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home.get_home),
+    path('', include('DLSCL.urls')),
     path('Login/', include('Login.urls'))
 ]
