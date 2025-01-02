c2cef6814f884f86e93f0eff0bdcb5a434535c91
Trungnq7474 <nguyenquangtrung0704@gmail.com>
2025-01-02 12:48:26 +0700

    đợt 5

--- mySite/settings.py
+++ mySite/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'mySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'root',
+        'PASSWORD': '0352493970',
+        'HOST': 'localhost',
     }
 }
 
