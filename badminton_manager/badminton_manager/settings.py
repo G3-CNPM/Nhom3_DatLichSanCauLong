5e9dcec35de5157988dac4518a9ab4fd2f717ff3
ky <dhoa8788@gmail.com>
2025-01-02 22:11:48 +0700

    ky

--- badminton_manager/badminton_manager/settings.py
+++ badminton_manager/badminton_manager/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'badminton_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'mysite',
+        'USER':'root',
+        'PASSWORD':'@Vanky012',
+        'HOST':'localhost',
     }
 }
 
