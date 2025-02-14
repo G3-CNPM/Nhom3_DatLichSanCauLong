2a5c75ca13711ac5479a94286ff4b23034e64f18
MinoReiC <minhnguyen15022005@gmail.com>
2025-02-14 21:24:44 +0700

    Lan thu 4

--- mySite/settings.py
+++ mySite/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = 'mySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysite',
-        'USER': 'root',
-        'PASSWORD': '0979222809Aa',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'mysite',
+        # 'USER': 'root',
+        # 'PASSWORD': '0979222809Aa',
+        # 'HOST': 'localhost',
     }
 }
 
