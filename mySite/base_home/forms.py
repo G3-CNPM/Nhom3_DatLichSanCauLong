bc8f460a262b55be1f730bd902b87220a7a60d51
MinoReiC <minhnguyen15022005@gmail.com>
2025-02-22 21:26:44 +0700

    Register

--- mySite/base_home/forms.py
+++ mySite/base_home/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Users
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
 
 class UserForm(ModelForm):
     class Meta:
--- mySite/base_home/views.py
+++ mySite/base_home/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import Users
 from .forms import UserForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -44,3 +48,24 @@ def voucher(request):
 
 def sanpham(request):
     return render(request, 'customer/sanpham.html')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have registered successfully! Welcome!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There wax a problem registering, please try again..."))
+            return redirect('register')
+    else:
+
+    return render(request, 'register.html', {'form':form})
